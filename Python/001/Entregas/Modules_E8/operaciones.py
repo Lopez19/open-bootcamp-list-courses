f92fd1d3e6c88ebfc2648b3197bc0c0f2ee6fc4a
Lopez19 <horaciolopm_2001@hotmail.com>
2023-01-18 19:07:22 -0500

    :sparkles: Add Entrega 8

---
+++ Python/001/Entregas/Modules_E8/operaciones.py
@@ -0,0 +1,14 @@
+def sumar(a, b):
+    return a + b
+
+
+def restar(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    return (a / b) if (b != 0) else "No se puede dividir por 0"
--- Python/001/Tipos de datos.py
+++ Python/001/Tipos de datos.py
@@ -7,18 +7,3 @@ listas = [1, 2, 3, 4, 5]
 tuplas = (1, 2, 3, 4, 5)
 diccionarios = {"nombre": "Horacio", "edad": 21}
 conjuntos = {1, 2, 3, 4, 5}
-
-# Métodos de los tipos de datos
-miTexto = "Hola, soy un texto"  # Definimos una variable de tipo texto
-miTexto.capitalize()  # Devuelve el texto con la primera letra en mayúsculas
-miTexto.title()  # Devuelve el texto con la primera letra de cada palabra en mayúsculas
-miTexto.lower()  # Devuelve el texto en minúsculas
-miTexto.upper()  # Devuelve el texto en mayúsculas
-miTexto.replace("Hola", "Adios")  # Reemplaza la palabra Hola por Adios
-miTexto.find("texto")  # Devuelve la posición de la primera letra del texto
-
-# Devuelve una lista con los elementos separados por la coma
-miTexto.split(",")
-
-# Combinados
-miTexto.replace(",", "").lower().lower().split()
